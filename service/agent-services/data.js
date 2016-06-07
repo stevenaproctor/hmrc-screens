8ef6e1d05cf822a8fa0c0a1240d0c2bf71f65db6
jay.alvarez <jay.alvarez@digital.hmrc.gov.uk>
2016-06-07 11:50:53 +0100

    added agent services folder and images to services folder for QA environment

---
+++ service/agent-services/data.js
@@ -0,0 +1,77 @@
+var data = {
+  "service": "18. Agent Services",
+  "last-updated": "7 June 2016",
+  "userjourneys": [
+    {
+      "title": "Live Service (QA environment - June 2016)",
+      "path": [{
+        "caption": "Advert to Switch",
+        "imgref": "images/web-qa-2016-06-07/1-switch-advert.png",
+        "note": "Agents are shown an advert to switch."
+      },{
+        "caption": "Promotional page",
+        "imgref": "images/web-qa-2016-06-07/2-promotional-page.png",
+        "note": "Agents are told about the new agent services and given a link to the sign in page."
+      },{
+        "caption": "Sign in page",
+        "imgref": "images/web-qa-2016-06-07/3-sign-in-page.png",
+        "note": "Agents sign in with their Government Gateway account."
+      },{
+        "caption": "Advert",
+        "imgref": "images/web-qa-2016-06-07/4-advert.png",
+        "note": "Agents who fulfil the criteria will be given the option to join the new service. <br /> Those who don't will be told why and taken to the existing service on the portal."
+      },{
+        "caption": "Advert: yes selected",
+        "imgref": "images/web-qa-2016-06-07/5-advert-yes-selected.png",
+        "note": "If agents choose to use the new service, they are told to make sure their client list is kept up to date to avoid Data Protection issues."
+      },{
+        "caption": "Agent's homepage",
+        "imgref": "images/web-qa-2016-06-07/6-agents-homepage.png",
+        "note": "The agent's homepage allows them to view, access and manage: <ul><li>services</li><li>messages</li><li>client registrations</li><li>account settings</li></ul> <strong>note:</strong> Agents services on the MDTP <br />only supports PAYE viewing of Allocations and Payments."
+      },{
+        "caption": "PAYE client list",
+        "imgref": "images/web-qa-2016-06-07/7-PAYE-client-list.png",
+        "note": "Agents can either confirm or remove clients from their list. An agent can only view the details of a confirmed client."
+      },{
+        "caption": "Confirming clients",
+        "imgref": "images/web-qa-2016-06-07/8-confirming-clients.png",
+        "note": "Once the client is confirmed, the button is replaced with a 'Confirmed' message and the client name becomes a link."
+      },{
+        "caption": "Removing client - message",
+        "imgref": "images/web-qa-2016-06-07/9-removing-client-message.png",
+        "note": "Confirms what happens when a client is removed and how to restore authorisation if necessary."
+      },{
+        "caption": "Search for a client",
+        "imgref": "images/web-qa-2016-06-07/10-client-search.png",
+        "note": "Allows agents to search for a specific client."
+      },{
+        "caption": "Client L&P account home",
+        "imgref": "images/web-qa-2016-06-07/11-client-summary.png",
+        "note": "Shows an overview of the client's payment history and each of their PAYE accounting periods."
+      },{
+        "caption": "Allocations tab",
+        "imgref": "images/web-qa-2016-06-07/12-allocations-tab.png",
+        "note": "Shows a summary list of client's allocations by month and year."
+      },{
+        "caption": "Allocations tab expanded",
+        "imgref": "images/web-qa-2016-06-07/13-allocations-tab-expanded.png",
+        "note": "Allows monthly allocations to be expanded to show further details."
+      },{
+        "caption": "Payments tab",
+        "imgref": "images/web-qa-2016-06-07/14-payments-tab.png",
+        "note": "Shows a summary of payments made by the client by month"
+        },{
+        "caption": "Payments tab expanded",
+        "imgref": "images/web-qa-2016-06-07/15-payments-tab-expanded.png",
+        "note": "Allows monthly payments to be expanded to show allocations and dates."
+      },{
+        "caption": "HMRC processing times",
+        "imgref": "images/web-qa-2016-06-07/16-HMRC-processing-times.png",
+        "note": "Gives agents a list of HMRC processing times."
+       },{
+        "caption": "Survey",
+        "imgref": "images/web-qa-2016-06-07/17-sign-out-survey.png",
+        "note": "Asks agents for feedback on using the service."
+      }]
+    }]
+}
