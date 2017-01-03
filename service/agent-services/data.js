f8a6ceef12e8f768aee6c50fce8c7d9cac97267c
Jay Alvarez <jay@Defaults-MacBook-Pro.local>
2017-01-03 17:02:49 +0000

    [JA][AOSS-1649]: add web chat screenshots

--- service/agent-services/data.js
+++ service/agent-services/data.js
@@ -1,72 +1,84 @@
 var data = {
   "service": "18. Agent Services",
-  "last-updated": "14 July 2016",
+  "last-updated": "3 Jan 2017",
   "userjourneys": [
     {
-      "title": "Live Service (QA environment - July 2016)",
+      "title": "Live Service (Mixed sources - Jan 2017)",
       "path": [{
         "caption": "Sign in page",
-        "imgref": "images/web-qa-2016-july/sign-in-page.png",
+        "imgref": "images/web-qa/sign-in-page.png",
         "note": "Agents sign in with their Government Gateway account."
       },{
         "caption": "Joining the new service",
-        "imgref": "images/web-qa-2016-july/joining-service.png",
+        "imgref": "images/web-qa/joining-service.png",
         "note": "Agents are asked if they want to switch to the new service. If they select ‘no’ they are redirected to HMRC online portal services."
       },{
         "caption": "Agent's homepage",
-        "imgref": "images/web-qa-2016-july/agents-homepage.png",
+        "imgref": "images/web-qa/agents-homepage.png",
         "note": "The agent's homepage allows them to view, access and manage: <ul><li>services</li><li>messages</li><li>client registrations</li><li>account settings</li></ul> <strong>note:</strong> Agents services on the digital tax platform <br />only supports PAYE viewing of Allocations and Payments."
       },{
         "caption": "Advert: yes selected",
-        "imgref": "images/web-qa-2016-july/switch-yes-selected.png",
+        "imgref": "images/web-qa/switch-yes-selected.png",
         "note": "If agents choose to use the new service, they are told to make sure their client list is kept up to date to avoid Data Protection issues."
       },{
         "caption": "PAYE client list",
-        "imgref": "images/web-qa-2016-july/PAYE-client-list.png",
+        "imgref": "images/web-qa/PAYE-client-list.png",
         "note": "Agents can either confirm or remove clients from their list. An agent can only view the details of a confirmed client."
       },{
         "caption": "Confirming clients",
-        "imgref": "images/web-qa-2016-july/confirming-clients.png",
+        "imgref": "images/web-qa/confirming-clients.png",
         "note": "Once the client is confirmed, the button is replaced with a 'Confirmed' message and the client name becomes a link."
       },{
         "caption": "Removing client - message",
-        "imgref": "images/web-qa-2016-july/removing-client-message.png",
+        "imgref": "images/web-qa/removing-client-message.png",
         "note": "Confirms what happens when a client is removed and how to restore authorisation if necessary."
       },{
         "caption": "Search for a client",
-        "imgref": "images/web-qa-2016-july/client-search.png",
+        "imgref": "images/web-qa/client-search.png",
         "note": "Allows agents to search for a specific client."
       },{
         "caption": "Client L&P statement",
-        "imgref": "images/web-qa-2016-july/client-summary.png",
+        "imgref": "images/web-qa/client-summary.png",
         "note": "Shows an overview of the client's payment history and each of their PAYE accounting periods."
       },{
         "caption": "Allocations tab",
-        "imgref": "images/web-qa-2016-july/allocations-tab.png",
+        "imgref": "images/web-qa/allocations-tab.png",
         "note": "Shows a summary list of client's allocations by month and year."
       },{
-				"caption": "Webchat popup",
-				"imgref": "images/web-qa-2016-july/webchat-popup.png",
-				"note": "Allocations tab (and the Payments tab) offer WebChat after 20 seconds. The screenshot illustrates a conversation taking place."
-			},{
+        "caption": "Webchat offer",
+        "imgref": "images/web-qa/allocations-chat-offer.JPG",
+        "note": "If an adviser is available for webchat - then the webchat pop up offer is displayed."
+      },{
+        "caption": "Webchat start chat",
+        "imgref": "images/web-qa/allocations-chat-start.JPG",
+        "note": "An agent can initiate webchat with the adviser by entering name and email and a message."
+      },{
+        "caption": "Webchat messages",
+        "imgref": "images/web-qa/allocations-chat-messages.JPG",
+        "note": "Agent and adviser can easily send and receive messages."
+      },{
+        "caption": "Webchat feedback",
+        "imgref": "images/web-qa/allocations-chat-feedback.JPG",
+        "note": "Once the chat has finished the agent is prompted to leave feedback on the service in a simple form  ."
+      },{
         "caption": "Allocations tab expanded",
-        "imgref": "images/web-qa-2016-july/allocations-tab-expanded.png",
+        "imgref": "images/web-qa/allocations-tab-expanded.png",
         "note": "Allows monthly allocations to be expanded to show further details."
       },{
         "caption": "Payments tab",
-        "imgref": "images/web-qa-2016-july/payments-tab.png",
+        "imgref": "images/web-qa/payments-tab.png",
         "note": "Shows a summary of payments made by the client by month"
         },{
         "caption": "Payments tab expanded",
-        "imgref": "images/web-qa-2016-july/payments-tab-expanded.png",
+        "imgref": "images/web-qa/payments-tab-expanded.png",
         "note": "Allows monthly payments to be expanded to show allocations and dates."
       },{
         "caption": "HMRC processing times",
-        "imgref": "images/web-qa-2016-july/HMRC-processing-times.png",
+        "imgref": "images/web-qa/HMRC-processing-times.png",
         "note": "Gives agents a list of HMRC processing times."
        },{
         "caption": "Survey",
-        "imgref": "images/web-qa-2016-july/sign-out-survey.png",
+        "imgref": "images/web-qa/sign-out-survey.png",
         "note": "Asks agents for feedback on using the service."
       }]
     },{
