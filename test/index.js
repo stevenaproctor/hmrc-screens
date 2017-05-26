e3afb4aefb0f7db9b02c541a286e0ae8ca3dfa8b
Rory Powis <rory.powis@digital.hmrc.gov.uk>
2017-05-26 22:18:05 +0200

    Refactor create service functions into module
    - Move functions in index.js to lib/create-service.js
    - Update function signature to accept all prompt args
    - Update tests to account for scenario name

--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@ var sinon = require('sinon')
 var rimraf = require('rimraf')
 var prompt = require('prompt')
 var getFile = require('./utils/get-file')
-var createNewFolder = require('../index').createNewFolder
+var createService = require('../lib/create-service')
 
 var cleanup = function (path) {
   rimraf.sync(path)
@@ -15,6 +15,8 @@ test('Creates a directory for a new service', function (t) {
   t.plan(4)
 
   var testDir = 'test-directory'
+  var testService = 'test-service'
+  var testScenario = 'test-scenario'
   var testDirPath = path.join(__dirname, '..', 'service', testDir)
   var dataFile = path.join(testDirPath, 'data.js')
 
@@ -23,15 +25,24 @@ test('Creates a directory for a new service', function (t) {
   })
 
   sinon.stub(console, 'log')
-  createNewFolder(testDir)
+  createService(testDir, testService, testScenario)
 
   getFile(dataFile, function (file) {
     if (file) {
       console.log.restore()
 
+      var data = {
+        'service': testService,
+        'last-updated': 'Some date',
+        'userjourneys': [{
+          'title': testScenario,
+          'path': []
+        }]
+      }
+
       var serviceDir = fs.readdirSync(testDirPath)
       var dataContents = fs.readFileSync(file).toString()
-      const expectedContent = 'var data = {"last-updated":"Some date","userjourneys":[{"path":[]}]}'
+      const expectedContent = 'var data = ' + JSON.stringify(data)
 
       t.true(serviceDir.includes('images'), 'with an images directory')
       t.true(serviceDir.includes('index.html'), 'with an index.html file')
