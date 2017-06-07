0af91f0a595c57f3d00a48277fbcc7a9acaf2bf6
Rory Powis <rory.powis@digital.hmrc.gov.uk>
2017-06-07 21:10:42 +0200

    Add a new scenario to an existing service's data file

--- test/build-data.js
+++ test/build-data.js
@@ -5,17 +5,18 @@ var sinon = require('sinon')
 var cleanup = require('./utils/cleanup')
 var buildData = require('../lib/build-data')
 
+var testDir = path.join(__dirname, 'service')
+var testService = 'test-service'
+var testScenario = 'test-scenario'
+var testScenarioTwo = 'test-scenario-two'
+
+var files = [
+  'not-an-image.txt',
+  '01-test-image-for-a-test-service.png'
+]
+
 test('Create a data file for a new service', function (t) {
   t.plan(1)
-
-  var testDir = path.join(__dirname, 'service')
-  var testService = 'test-service'
-  var testScenario = 'test-scenario'
-
-  var files = [
-    'not-an-image.txt',
-    '01-test-image-for-a-test-service.png'
-  ]
 
   var data = {
     'service': testService,
@@ -35,14 +36,60 @@ test('Create a data file for a new service', function (t) {
 
   sinon.stub(console, 'log')
 
-  buildData(files, testDir, testService, testScenario).then(function (filePath) {
-    console.log.restore()
+  buildData(files, testDir, testService, testScenario)
+    .then(function (filePath) {
+      console.log.restore()
 
-    var dataContents = fs.readFileSync(filePath).toString()
-    const expectedContents = 'var data = ' + JSON.stringify(data, null, 2)
+      var dataContents = fs.readFileSync(filePath).toString()
+      const expectedContents = 'var data = ' + JSON.stringify(data, null, 2)
 
-    t.equal(dataContents, expectedContents, 'with a data object built from args')
+      t.equal(dataContents, expectedContents, 'with a data object built from args')
 
-    cleanup(testDir)
-  })
+      cleanup(testDir)
+    })
+})
+
+test('Create a data file for a new scenario in an existing service', function (t) {
+  t.plan(1)
+
+  var data = {
+    'service': testService,
+    'last-updated': 'Some date',
+    'userjourneys': [{
+      'title': testScenario,
+      'path': [{
+        'caption': 'test image for a test service',
+        'imgref': 'images/' + testScenario + '/' + files[1],
+        'note': 'Notes go here...'
+      }]
+    },
+    {
+      'title': testScenarioTwo,
+      'path': [{
+        'caption': 'test image for a test service',
+        'imgref': 'images/' + testScenarioTwo + '/' + files[1],
+        'note': 'Notes go here...'
+      }]
+    }]
+  }
+
+  cleanup(testDir)
+  fs.mkdirSync(testDir)
+
+  sinon.stub(console, 'log')
+
+  buildData(files, testDir, testService, testScenario)
+    .then(function () {
+      return buildData(files, testDir, testService, testScenarioTwo)
+    })
+    .then(function (filePath) {
+      console.log.restore()
+
+      var dataContents = fs.readFileSync(filePath).toString()
+      const expectedContents = 'var data = ' + JSON.stringify(data, null, 2)
+
+      t.equal(dataContents, expectedContents, 'with a data object built from args')
+
+      cleanup(testDir)
+    })
 })
