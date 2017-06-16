a5849a7af34bfbe680424af1b07a57c5b95c061f
robert.lavender <robert.lavender@digital.hmrc.gov.uk>
2017-06-16 12:03:24 +0100

    Add Tax Calculation Overpayment journeys.

---
+++ service/tax-calculation--overpayment/data.js
@@ -0,0 +1,241 @@
+var data = {
+  "service": "Tax Calculation Overpayment",
+  "last-updated": "Some date",
+  "userjourneys": [
+    {
+      "title": "Available without supplement",
+      "path": [
+        {
+          "caption": "check income tax last year",
+          "imgref": "images/available-without-supplement/02-check-income-tax-last-year.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "paid too much",
+          "imgref": "images/available-without-supplement/03-paid-too-much.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "why you owe tax",
+          "imgref": "images/available-without-supplement/01-why-you-owe-tax.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "calculation summary",
+          "imgref": "images/available-without-supplement/04-calculation-summary.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "get your refund",
+          "imgref": "images/available-without-supplement/05-get-your-refund.png",
+          "note": "Notes go here..."
+        }
+      ]
+    },
+    {
+      "title": "Processing without supplement",
+      "path": [
+        {
+          "caption": "check income tax last year",
+          "imgref": "images/processing-without-supplement/02-check-income-tax-last-year.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "paid too much",
+          "imgref": "images/processing-without-supplement/03-paid-too-much.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "why you owe tax",
+          "imgref": "images/processing-without-supplement/04-why-you-owe-tax.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "calculation summary",
+          "imgref": "images/processing-without-supplement/01-calculation-summary.png",
+          "note": "Notes go here..."
+        }
+      ]
+    },
+    {
+      "title": "Cheque sent without supplement",
+      "path": [
+        {
+          "caption": "check income tax last year",
+          "imgref": "images/cheque-sent-without-supplement/02-check-income-tax-last-year.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "paid too much",
+          "imgref": "images/cheque-sent-without-supplement/03-paid-too-much.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "why you owe tax",
+          "imgref": "images/cheque-sent-without-supplement/04-why-you-owe-tax.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "calculation summary",
+          "imgref": "images/cheque-sent-without-supplement/01-calculation-summary.png",
+          "note": "Notes go here..."
+        }
+      ]
+    },
+    {
+      "title": "Claimed without supplement",
+      "path": [
+        {
+          "caption": "check income tax last year",
+          "imgref": "images/claimed-without-supplement/01-check-income-tax-last-year.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "paid too much",
+          "imgref": "images/claimed-without-supplement/02-paid-too-much.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "why you owe tax",
+          "imgref": "images/claimed-without-supplement/03-why-you-owe-tax.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "calculation summary",
+          "imgref": "images/claimed-without-supplement/04-calculation-summary.png",
+          "note": "Notes go here..."
+        }
+      ]
+    },
+    {
+      "title": "Not available without supplement",
+      "path": [
+        {
+          "caption": "check income tax last year",
+          "imgref": "images/not-available-without-supplement/02-check-income-tax-last-year.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "paid too much",
+          "imgref": "images/not-available-without-supplement/03-paid-too-much.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "why you owe tax",
+          "imgref": "images/not-available-without-supplement/04-why-you-owe-tax.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "calculation summary",
+          "imgref": "images/not-available-without-supplement/01-calculation-summary.png",
+          "note": "Notes go here..."
+        }
+      ]
+    },
+    {
+      "title": "Available with supplement",
+      "path": [
+        {
+          "caption": "check income tax last year",
+          "imgref": "images/available-with-supplement/01-check-income-tax-last-year.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "paid too much",
+          "imgref": "images/available-with-supplement/03-paid-too-much.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "why you owe tax",
+          "imgref": "images/available-with-supplement/04-why-you-owe-tax.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "get your refund",
+          "imgref": "images/available-with-supplement/02-get-your-refund.png",
+          "note": "Notes go here..."
+        }
+      ]
+    },
+    {
+      "title": "Processing with supplement",
+      "path": [
+        {
+          "caption": "check income tax last year",
+          "imgref": "images/processing-with-supplement/02-check-income-tax-last-year.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "paid too much",
+          "imgref": "images/processing-with-supplement/01-paid-too-much.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "why you owe tax",
+          "imgref": "images/processing-with-supplement/03-why-you-owe-tax.png",
+          "note": "Notes go here..."
+        }
+      ]
+    },
+    {
+      "title": "Cheque sent with supplement",
+      "path": [
+        {
+          "caption": "check income tax last year",
+          "imgref": "images/cheque-sent-with-supplement/02-check-income-tax-last-year.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "paid too much",
+          "imgref": "images/cheque-sent-with-supplement/01-paid-too-much.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "why you owe tax",
+          "imgref": "images/cheque-sent-with-supplement/03-why-you-owe-tax.png",
+          "note": "Notes go here..."
+        }
+      ]
+    },
+    {
+      "title": "Claimed with supplement",
+      "path": [
+        {
+          "caption": "check income tax last year",
+          "imgref": "images/claimed-with-supplement/02-check-income-tax-last-year.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "paid too much",
+          "imgref": "images/claimed-with-supplement/01-paid-too-much.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "why you owe tax",
+          "imgref": "images/claimed-with-supplement/03-why-you-owe-tax.png",
+          "note": "Notes go here..."
+        }
+      ]
+    },
+    {
+      "title": "Not available with supplement",
+      "path": [
+        {
+          "caption": "check income tax last year",
+          "imgref": "images/not-available-with-supplement/01-check-income-tax-last-year.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "paid too much",
+          "imgref": "images/not-available-with-supplement/02-paid-too-much.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "why you owe tax",
+          "imgref": "images/not-available-with-supplement/03-why-you-owe-tax.png",
+          "note": "Notes go here..."
+        }
+      ]
+    }
+  ]
+}
