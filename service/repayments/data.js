a9a0cc91a526f29b53671efbd1cfa4ec3b340cc4
robert.lavender <robert.lavender@digital.hmrc.gov.uk>
2017-06-26 10:51:02 +0100

    Add Repayments service.

---
+++ service/repayments/data.js
@@ -0,0 +1,186 @@
+var data = {
+  "service": "Repayments",
+  "last-updated": "26th June 2017",
+  "userjourneys": [
+    {
+      "title": "Customer accepts UK address",
+      "path": [
+        {
+          "caption": "check income tax last year",
+          "imgref": "images/customer-accepts-uk-address/01-check-income-tax-last-year.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "you paid too much income tax",
+          "imgref": "images/customer-accepts-uk-address/02-you-paid-too-much-income-tax.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "repayment start",
+          "imgref": "images/customer-accepts-uk-address/03-repayment-start.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "your bank account details",
+          "imgref": "images/customer-accepts-uk-address/04-your-bank-account-details.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "your address",
+          "imgref": "images/customer-accepts-uk-address/05-your-address.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "check details",
+          "imgref": "images/customer-accepts-uk-address/06-check-details.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "request submitted",
+          "imgref": "images/customer-accepts-uk-address/07-request-submitted.png",
+          "note": "Notes go here..."
+        }
+      ]
+    },
+    {
+      "title": "Customer chooses someone elses account",
+      "path": [
+        {
+          "caption": "check income tax last year",
+          "imgref": "images/customer-chooses-someone-elses-account/01-check-income-tax-last-year.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "you paid too much income tax",
+          "imgref": "images/customer-chooses-someone-elses-account/02-you-paid-too-much-income-tax.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "repayment start",
+          "imgref": "images/customer-chooses-someone-elses-account/03-repayment-start.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "someone elses bank account details",
+          "imgref": "images/customer-chooses-someone-elses-account/04-someone-elses-bank-account-details.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "known at UK address",
+          "imgref": "images/customer-chooses-someone-elses-account/05-known-at-UK-address.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "edit address",
+          "imgref": "images/customer-chooses-someone-elses-account/06-edit-address.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "check details",
+          "imgref": "images/customer-chooses-someone-elses-account/07-check-details.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "request submitted",
+          "imgref": "images/customer-chooses-someone-elses-account/08-request-submitted.png",
+          "note": "Notes go here..."
+        }
+      ]
+    },
+    {
+      "title": "Customer edits UK address",
+      "path": [
+        {
+          "caption": "check income tax last year",
+          "imgref": "images/customer-edits-uk-address/01-check-income-tax-last-year.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "you paid too much income tax",
+          "imgref": "images/customer-edits-uk-address/02-you-paid-too-much-income-tax.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "repayment start",
+          "imgref": "images/customer-edits-uk-address/03-repayment-start.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "your bank account details",
+          "imgref": "images/customer-edits-uk-address/04-your-bank-account-details.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "your address",
+          "imgref": "images/customer-edits-uk-address/05-your-address.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "known at UK address",
+          "imgref": "images/customer-edits-uk-address/06-known-at-UK-address.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "edit address",
+          "imgref": "images/customer-edits-uk-address/07-edit-address.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "check details",
+          "imgref": "images/customer-edits-uk-address/08-check-details.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "request submitted",
+          "imgref": "images/customer-edits-uk-address/09-request-submitted.png",
+          "note": "Notes go here..."
+        }
+      ]
+    },
+    {
+      "title": "Customer has foreign address",
+      "path": [
+        {
+          "caption": "check income tax last year",
+          "imgref": "images/customer-has-foreign-address/01-check-income-tax-last-year.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "you paid too much income tax",
+          "imgref": "images/customer-has-foreign-address/02-you-paid-too-much-income-tax.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "repayment start",
+          "imgref": "images/customer-has-foreign-address/03-repayment-start.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "your bank account details",
+          "imgref": "images/customer-has-foreign-address/04-your-bank-account-details.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "your address",
+          "imgref": "images/customer-has-foreign-address/05-your-address.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "known at UK address",
+          "imgref": "images/customer-has-foreign-address/06-known-at-UK-address.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "check details",
+          "imgref": "images/customer-has-foreign-address/07-check-details.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "request submitted",
+          "imgref": "images/customer-has-foreign-address/08-request-submitted.png",
+          "note": "Notes go here..."
+        }
+      ]
+    }
+  ]
+}
