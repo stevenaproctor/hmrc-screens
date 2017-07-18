f04a3c80f7902822b0625c8d5d5cf2f79c91a8b8
Ian Leggett <ileggett@equalexperts.com>
2017-07-18 11:46:13 +0100

    Added new service: Tax credits change of circumstances

---
+++ service/tax-credits-change-of-circumstances/data.js
@@ -0,0 +1,46 @@
+var data = {
+  "service": "Tax credits change of circumstances",
+  "last-updated": "18th July 2017",
+  "userjourneys": [
+    {
+      "title": "Change your name",
+      "path": [
+        {
+          "caption": "Tax credits summary",
+          "imgref": "images/change-your-name/01-Tax-credits-summary.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "Personal details",
+          "imgref": "images/change-your-name/02-Personal-details.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "Do you need to change your name",
+          "imgref": "images/change-your-name/03-Do-you-need-to-change-your-name.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "What is your name",
+          "imgref": "images/change-your-name/04-What-is-your-name.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "Confirm your answers",
+          "imgref": "images/change-your-name/05-Confirm-your-answers.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "Change received",
+          "imgref": "images/change-your-name/06-Change-received.png",
+          "note": "Notes go here..."
+        },
+        {
+          "caption": "Personal details after change",
+          "imgref": "images/change-your-name/07-Personal-details-after-change.png",
+          "note": "Notes go here..."
+        }
+      ]
+    }
+  ]
+}
